4748c5e0d45a10f31aec38a6e7cec38f5680153a
Jack21369 <jlee21369@gmail.com>
2025-03-24 23:48:43 -0700

    draft v5: added footer, responsive on mobile+desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
